a868028699d57f5f2019b39e885b3064d9f71e92
Mateo Astelarra <mateo@astelarra.com>
2023-12-08 20:03:40 -0300

    Added object pool for bullets

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour
     [SerializeField] private int _damageAmount = 1;
 
     private Vector2 _fireDirection;
-
+    private Gun _gun;
     private Rigidbody2D _rigidBody;
 
     private void Awake()
@@ -16,8 +16,10 @@ public class Bullet : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    public void Init(Vector2 bulletSpawnPos, Vector2 mousePos)
+    public void Init(Gun gun, Vector2 bulletSpawnPos, Vector2 mousePos)
     {
+        _gun = gun;
+        transform.position = bulletSpawnPos;
         _fireDirection = (mousePos - bulletSpawnPos).normalized;
     }
 
@@ -29,6 +31,6 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         Health health = other.gameObject.GetComponent<Health>();
         health?.TakeDamage(_damageAmount);
-        Destroy(this.gameObject);
+        _gun.ReleaseBulletFromPool(this);
     }
 }
--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,40 +1,62 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Gun : MonoBehaviour
 {
     public static Action OnShoot;
-    public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
     [SerializeField] private float _gunFireCD = 0.5f;
 
+    private ObjectPool<Bullet> _bulletPool;
     private static readonly int FIRE_HASH = Animator.StringToHash("Fire");
     private Vector2 _mousePos;
-    private float _lastFireTime;
+    private float _lastFireTime = 0f;
 
     private Animator _animator;
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();     
+    }
+
+    private void Start()
+    {
+        CreateBulletPool();
     }
 
     private void OnEnable()
     {
         OnShoot += ShootProjectile;
-        OnShoot += FireAnimation;
         OnShoot += UpdateLastFireTime;
+        OnShoot += FireAnimation;   
     }
 
     private void OnDisable()
     {
         OnShoot -= ShootProjectile;
-        OnShoot -= FireAnimation;
         OnShoot -= UpdateLastFireTime;
+        OnShoot -= FireAnimation;     
+    }
+
+    public void ReleaseBulletFromPool(Bullet bullet)
+    {
+        _bulletPool.Release(bullet);
+    }
+
+    private void CreateBulletPool()
+    {
+        _bulletPool = new ObjectPool<Bullet>(() => {
+            return Instantiate(_bulletPrefab);
+        }, bullet => {
+            bullet.gameObject.SetActive(true);
+        }, bullet => {
+            bullet.gameObject.SetActive(false);
+        }, bullet => {
+            Destroy(bullet);
+        }, false, 20, 40);
     }
 
     private void Update()
@@ -52,14 +74,14 @@ public class Gun : MonoBehaviour
     }
 
     private void ShootProjectile()
-    { 
-        Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
-        newBullet.Init(_bulletSpawnPoint.position, _mousePos);
+    {
+        Bullet newBullet = _bulletPool.Get();
+        newBullet.Init(this, _bulletSpawnPoint.position, _mousePos);
     }
 
     private void UpdateLastFireTime()
     {
-        _lastFireTime += _gunFireCD;
+        _lastFireTime = Time.time + _gunFireCD;
     }
 
     private void FireAnimation()
