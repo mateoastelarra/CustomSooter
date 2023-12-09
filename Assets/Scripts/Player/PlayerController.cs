b30addbc6acd33c05ab10515e799791a5ceba39f
Mateo Astelarra <mateo@astelarra.com>
2023-12-09 09:44:38 -0300

    Adjusted jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
 
+    [SerializeField] private Transform _feetTransform;
+    [SerializeField] private Vector2 _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _jumpStrength = 7f;
 
@@ -31,20 +34,17 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    
+    private bool CheckGrounded()
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isGrounded = true;
-        }
+        Collider2D isGrounded = Physics2D.OverlapBox(_feetTransform.position, _groundCheck, 0, _groundLayer);
+        return isGrounded;
     }
 
-    private void OnCollisionExit2D(Collision2D other)
+    private void OnDrawGizmos()
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isGrounded = false;
-        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(_feetTransform.position, _groundCheck);
     }
 
     public bool IsFacingRight()
@@ -65,7 +65,7 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && CheckGrounded()) {
             _rigidBody.AddForce(Vector2.up * _jumpStrength, ForceMode2D.Impulse);
         }
     }
