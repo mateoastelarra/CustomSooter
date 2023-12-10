c332e0f74ab7ac6598c6d89a542f4e13dbc36c31
Mateo Astelarra <mateo@astelarra.com>
2023-12-10 14:13:03 -0300

    Added Death VFX for enemies

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,16 +1,32 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Health : MonoBehaviour
 {
+    public Action OnDeath;
+
     [SerializeField] private GameObject _splatterPrefab;
+    [SerializeField] private GameObject _deathVFXPrefab;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
 
     private void Start() {
         ResetHealth();
+    }
+
+    private void OnEnable()
+    {
+        OnDeath += SpawnDeathSplatterPrefab;
+        OnDeath += SpawnDeathVFXPrefab;
+    }
+
+    private void OnDisable()
+    {
+        OnDeath -= SpawnDeathSplatterPrefab;
+        OnDeath -= SpawnDeathVFXPrefab;
     }
 
     public void ResetHealth() {
@@ -21,7 +37,7 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
-            SpawnDeathSplatterPrefab();
+            OnDeath?.Invoke();
             Destroy(gameObject);
         }
     }
@@ -32,5 +48,13 @@ public class Health : MonoBehaviour
         SpriteRenderer splatterSR = newSplatterPrefab.GetComponent<SpriteRenderer>();
         ColorChanger enemyColorChanger = GetComponent<ColorChanger>();
         splatterSR.color = enemyColorChanger.DefaultColor;
+    }
+
+    public void SpawnDeathVFXPrefab()
+    {
+        GameObject newDeathVFX = Instantiate(_deathVFXPrefab, transform.position, transform.rotation);
+        ParticleSystem.MainModule psMainModule = newDeathVFX.GetComponent<ParticleSystem>().main;
+        ColorChanger enemyColorChanger = GetComponent<ColorChanger>();
+        psMainModule.startColor = enemyColorChanger.DefaultColor;
     }
 }
