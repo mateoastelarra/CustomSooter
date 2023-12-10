1c23fa0c464d0082636a16cee974ae6a497c9308
Mateo Astelarra <mateo@astelarra.com>
2023-12-10 13:42:22 -0300

    Death Splatter effect

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private GameObject _splatterPrefab;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
@@ -20,7 +21,16 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
+            SpawnDeathSplatterPrefab();
             Destroy(gameObject);
         }
+    }
+
+    public void SpawnDeathSplatterPrefab()
+    {
+        GameObject newSplatterPrefab = Instantiate(_splatterPrefab, transform.position, transform.rotation);
+        SpriteRenderer splatterSR = newSplatterPrefab.GetComponent<SpriteRenderer>();
+        ColorChanger enemyColorChanger = GetComponent<ColorChanger>();
+        splatterSR.color = enemyColorChanger.DefaultColor;
     }
 }
