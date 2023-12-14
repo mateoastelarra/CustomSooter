d5b005cfeb34472e019963e11be62a7934a1b29d
Mateo Astelarra <mateo@astelarra.com>
2023-12-14 10:43:51 -0300

    One way Platform

---
+++ Assets/Scripts/Misc/OneWayPlatform.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+    private bool _playerOnPlatform = false;
+    private float _disableColliderTime = 1f;
+    private Collider2D _platformCollider;
+
+    Coroutine _disablePlatformCollider;
+
+    private void Awake()
+    {
+        _platformCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        DetectPlayerInput();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>())
+        {
+            _playerOnPlatform = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            _playerOnPlatform = false;
+        }
+    }
+
+    private void DetectPlayerInput()
+    {
+        if (!_playerOnPlatform) { return; }
+        if(_disablePlatformCollider != null) { return; }
+
+        if (PlayerController.Instance.MoveInput.y < 0)
+        {
+            _disablePlatformCollider = StartCoroutine(DisablePlatformCollider());
+        }
+    }
+
+    private IEnumerator DisablePlatformCollider()
+    {
+        Collider2D[] playerColliders = PlayerController.Instance.GetComponents<Collider2D>();
+
+        foreach(Collider2D playerCollider in playerColliders)
+        {
+            Physics2D.IgnoreCollision(playerCollider, _platformCollider, true);
+        }
+
+        yield return new WaitForSeconds(_disableColliderTime);
+
+        foreach (Collider2D playerCollider in playerColliders)
+        {
+            Physics2D.IgnoreCollision(playerCollider, _platformCollider, false);
+        }
+        _disablePlatformCollider = null;
+    }
+}
