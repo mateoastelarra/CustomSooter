2e8bbbccfb1ae9e14189f7c59a0f0a3eb1a1e7df
Mateo Astelarra <mateo@astelarra.com>
2023-12-17 16:32:38 -0300

    Can destroy Pipes

--- Assets/Scripts/Enemy/Pipe.cs
+++ Assets/Scripts/Enemy/Pipe.cs
@@ -1,24 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Pipe : MonoBehaviour
+public class Pipe : MonoBehaviour, IDamageable
 {
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private float _spawnTimer = 3f;
 
     private ColorChanger _colorChanger;
+    private Health _health;
+    private Flash _flash;
 
     private void Awake()
     {
         _colorChanger = GetComponent<ColorChanger>();
+        _health = GetComponent<Health>();
+        _flash = GetComponent<Flash>();
     }
 
-    private void Start() {
+    private void Start()
+    {
         StartCoroutine(SpawnRoutine());
     }
-    
-    private IEnumerator SpawnRoutine() {
+
+    private IEnumerator SpawnRoutine()
+    {
         while (true)
         {
             _colorChanger.SetRandomColor();
@@ -26,5 +31,15 @@ public class Pipe : MonoBehaviour
             enemy.Init(_colorChanger.DefaultColor);
             yield return new WaitForSeconds(_spawnTimer);
         }
+    }
+
+    public void TakeDamage(int damageAmount, float knockbackTrhust)
+    {
+        _health.TakeDamage(damageAmount);
+    }
+
+    public void TakeHit()
+    {
+        _flash.StartFlash();
     }
 }
