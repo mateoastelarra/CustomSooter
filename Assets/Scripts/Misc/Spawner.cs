ebbc7aa7b488f4d7ddf6656eb1eb71d83963bef2
Mateo Astelarra <mateo@astelarra.com>
2023-12-20 09:55:29 -0300

    Player random spawnpoint

--- Assets/Scripts/Misc/Spawner.cs
+++ Assets/Scripts/Misc/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _spawnable; 
-    [SerializeField] private GameObject[] _spawnPoints;
+    [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] [Range(0, 20)] int _spawnquantity;
 
 
@@ -22,10 +22,10 @@ public class Spawner : MonoBehaviour
         while (spawnedObjects < _spawnquantity)
         {
             float randomNum = Random.Range(0f, 1f);
-            if (randomNum < 1 / (_spawnquantity * 1.0f) && !_spawnPoints[spawnPosition].activeInHierarchy)
+            if (randomNum < 1 / (_spawnquantity * 1.0f) && _spawnPoints[spawnPosition].childCount == 0)
             {
-                //Instantiate(_spawnable, _spawnPoints[spawnPosition].position, Quaternion.identity, transform);
-                _spawnPoints[spawnPosition].SetActive(true);
+                Instantiate(_spawnable, _spawnPoints[spawnPosition].position, Quaternion.identity, _spawnPoints[spawnPosition]);
+                //_spawnPoints[spawnPosition].SetActive(true);
                 spawnedObjects++;
                 Debug.Log(spawnPosition);
             }
